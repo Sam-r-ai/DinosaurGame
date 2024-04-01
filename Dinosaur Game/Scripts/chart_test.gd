a18ae5e85dcdc4e35e92f9ed05cbef0338f9fa84
dingus-wingus <161263046+dingus-wingus@users.noreply.github.com>
2024-04-01 00:33:05 -0700

    JSON saves!!!!!
    saving in the chart editor now creates a chart_saves.bin file that can be loaded in another session! atm you can only have one though, next up will be multiple charts!

--- Dinosaur Game/Scripts/chart_test.gd
+++ Dinosaur Game/Scripts/chart_test.gd
@@ -1,6 +1,6 @@
 extends Node2D;
 
-class_name Chart;
+const SAVE_PATH = "res://chart_saves.bin";
 
 @export var chart_editor : ChartEditor;
 @export var note_parent : Node2D;
@@ -12,32 +12,56 @@ class_name Chart;
 var note_dictionary = {};
 
 func save_chart():
+	var chart_file = FileAccess.open(SAVE_PATH, FileAccess.WRITE);
+	
 	var note_count : int = 0;
 	note_dictionary.clear();
 	
 	for note : ChartNote in note_parent.get_children():
 		
 		var index : int = note_dictionary.size();
-		var note_data = []
+		var note_data : Array = [str(note.lane), str(note.timestamp)]
 		
-		note_data = [Vector2(note.lane, note.timestamp)];
 		note_dictionary[index] = note_data;
+	
+	var jstr = JSON.stringify(note_dictionary);
+	
+	chart_file.store_line(jstr);
+	
 
 func load_chart():
+	var chart_file = FileAccess.open(SAVE_PATH, FileAccess.READ);	
+	if FileAccess.file_exists(SAVE_PATH):
+		if !chart_file.eof_reached():
+			var data = JSON.parse_string(chart_file.get_line());
+			if data:
+				if typeof(data) == TYPE_DICTIONARY:
+					note_dictionary = data;
+					print(note_dictionary);
+				else:
+					print("invalid type");
+			else:
+				print("data = null");
+	
 	clear_chart();
 	
+	
 	for note in note_dictionary:
+		if !typeof(note_dictionary[note]) == TYPE_ARRAY:
+			print("not array");
+		
 		var note_data = note_dictionary[note];
-		print(note_data[0]);
+		
+		note_data[0] = note_data[0].to_float();
+		note_data[1] = note_data[1].to_float();
 		
 		var new_note = chart_editor.note_prefab.instantiate();
-		new_note.global_position.x = note_data[0].x*grid.SPACE_SIZE.x;
-		new_note.global_position.y = note_data[0].y * grid.SPACE_SIZE.y*chart_editor.beats_per_second*chart_editor.grid_spaces_per_beat *-1;
+		new_note.global_position.x = note_data[0] * grid.SPACE_SIZE.x;
+		new_note.global_position.y = note_data[1] * grid.SPACE_SIZE.y*chart_editor.beats_per_second*chart_editor.grid_spaces_per_beat *-1;
 		note_parent.add_child(new_note);
 		
-		new_note.lane = note_data[0].x;
-		new_note.timestamp = note_data[0].y;
-	
+		new_note.lane = note_data[0];
+		new_note.timestamp = note_data[1];
 
 func clear_chart():
 	for note in note_parent.get_children():
